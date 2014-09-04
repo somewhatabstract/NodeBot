3a23efe98fcae9c0810ce2097804c11f2acf12fa
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2014-09-04 15:11:48 -0400

    Added speed and keyboard control

--- bot.js
+++ bot.js
@@ -5,9 +5,8 @@ var root = __dirname,
     board = new five.Board();
 
 board.on("ready", function() {
-
 	var drive = createDrive();
-
+	board.repl.inject({drive:drive});
 	initializeApi(drive);
 });
 
@@ -15,56 +14,56 @@ function createDrive() {
 	var drive = {
 		left: createServo(3,false),
 		right: createServo(10,true),
-		forward: function (foot) {
+		forward: function (foot, rate) {
 			switch (foot)
 			{
 			case 'both':
-				drive.left.forward()
-				drive.right.forward()
+				drive.left.forward(rate)
+				drive.right.forward(rate)
 				break;
 
 			case 'left':
 				drive.right.stop()
-				drive.left.forward()
+				drive.left.forward(rate)
 				break;
 				
 			case 'right':
 				drive.left.stop()
-				drive.right.forward()
+				drive.right.forward(rate)
 				break;
 			}
 		},
-		back: function (foot) {
+		back: function (foot, rate) {
 			switch (foot)
 			{
 			case 'both':
-				drive.left.back()
-				drive.right.back()
+				drive.left.back(rate)
+				drive.right.back(rate)
 				break;
 
 			case 'left':
 				drive.right.stop()
-				drive.left.back()
+				drive.left.back(rate)
 				break;
 				
 			case 'right':
 				drive.left.stop()
-				drive.right.back()
+				drive.right.back(rate)
 				break;
 			}
 		},
-		rotate: function (direction)
+		rotate: function (direction, rate)
 		{
 			switch (direction)
 			{
 			case 'counterclockwise':
-				drive.left.back()
-				drive.right.forward()
+				drive.left.back(rate)
+				drive.right.forward(rate)
 				break;
 
 			case 'clockwise':
-				drive.left.forward()
-				drive.right.back()
+				drive.left.forward(rate)
+				drive.right.back(rate)
 				break;
 			}
 		},
@@ -77,6 +76,7 @@ function createDrive() {
 }
 
 function createServo(pin, invert) {
+	var topSpeed = 90;
 	var servo = new five.Servo({
 		pin: pin,
 		isInverted: invert,
@@ -84,8 +84,14 @@ function createServo(pin, invert) {
 		specs: { speed: five.Servo.Continuous.speeds["@5.0V"] }
 	});
 
-	servo.forward = function() { servo.to(130); };
-	servo.back = function() { servo.to(50); };
+	servo.forward = function(rate) {
+		var speed = 90 + ((rate || 1) * topSpeed);
+		servo.to(speed);
+	};
+	servo.back = function(rate) {
+		var speed = 90 - ((rate || 1) * topSpeed);
+		servo.to(speed);
+	};
 
 	servo.stop();
 
@@ -97,13 +103,13 @@ function initializeApi(drive) {
 
 	app.post('/move', function (req, res){
 		var direction = req.param('direction');
-		drive[req.param('direction')](req.param('foot'));
+		drive[req.param('direction')](req.param('foot'), req.param('rate'));
 		res.send();
 	});
 
 	app.post('/rotate', function (req, res){
 		var direction = req.param('direction');
-		drive.rotate(direction);
+		drive.rotate(direction, req.param('rate'));
 		res.send();
 	});
 
--- public/js/main.js
+++ public/js/main.js
@@ -8,39 +8,112 @@ $(function(){
 	module.factory('nbApi', ['$resource', function ($resource){
 		return $resource(
 			'/:action',
-			{ direction: '@direction', foot: '@foot' },
+			{ direction: '@direction', foot: '@foot', rate: '@rate' },
 			{
-				rotate: {method:'POST', params: {action: 'rotate'}},
-				stop: {method:'POST', params: {action: 'stop'}},
-				forward: {method:'POST', params: {action:'move', direction:'forward'}},
-				back: {method:'POST', params: {action:'move', direction:'back'}}
+				rotate: {method:'POST', params: {action: 'rotate', hacker: 'notahacker'}},
+				stop: {method:'POST', params: {action: 'stop', hacker: 'notahacker'}},
+				forward: {method:'POST', params: {action:'move', direction:'forward', hacker: 'notahacker'}},
+				back: {method:'POST', params: {action:'move', direction:'back', hacker: 'notahacker'}}
 			}
 		);
 	}]);
 
-	module.controller('botController', ['$q', '$scope', 'nbApi', function($q, $scope, nbApi){
+	module.controller('botController', ['$rootScope', '$scope', 'nbApi', function($rootScope, $scope, nbApi){
 		$scope.status = "Not Initialized";
 
+		var lastAction = angular.noop;
+
+		function performAndSave(action)	{
+			lastAction = action;
+			action();
+		};
+
 		$scope.rotate = function(direction)	{
-			nbApi.rotate({direction: direction});
+			performAndSave( function() {
+				nbApi.rotate({direction: direction, rate: $scope.rate});
+			} );
 		};
 
 		$scope.stop = function() {
-			nbApi.stop();
+			performAndSave( function() {
+				nbApi.stop();
+			} );
 		};
 
 		$scope.forward = function(foot) {
-			nbApi.forward({foot: foot});
+			performAndSave( function() {
+				nbApi.forward({foot: foot, rate: $scope.rate});
+			} );
 		};
 
 		$scope.back = function(foot) {
-			nbApi.back({foot: foot});
+			performAndSave( function() {
+				nbApi.back({foot: foot, rate: $scope.rate});
+			} );
+		};
+
+		$scope.goSlow = function() {
+			$scope.rate = 0.05;
+			lastAction();
+		};
+
+		$scope.goFast = function() {
+			$scope.rate = 1;
+			lastAction();
 		};
 		
-		$scope.$on('keyup', function() {
-		} );
+		$scope.onKeyup = function($event) {
+			switch ($event.keyCode)
+			{
+			case 38:
+			case 40:
+				break;
+			
+			default:
+				$scope.stop();
+				break;
+			}
+		};
 
-		$scope.$on('keydown', function() {
-		} );
+		$scope.onKeydown = function($event) {
+			switch ($event.keyCode)
+			{
+			case 87: //W
+				$scope.forward('both');
+				break;
+
+			case 83: //S
+				$scope.back('both');
+				break;
+
+			case 65: //A
+				if ($event.shiftKey) {
+					$scope.rotate('counterclockwise');
+				} else {
+					$scope.forward('right');
+				}
+				break;
+
+			case 68: //D
+				if ($event.shiftKey) {
+					$scope.rotate('clockwise');
+				} else {
+					$scope.forward('left');
+				}
+				break;
+
+			case 32: //SPACE
+				$scope.stop();
+				break;
+
+			case 40: //DOWN
+				$scope.goSlow();
+				break;
+
+			case 38: //UP
+				$scope.goFast();
+				break;
+			}
+		};
 	}]);
 });
